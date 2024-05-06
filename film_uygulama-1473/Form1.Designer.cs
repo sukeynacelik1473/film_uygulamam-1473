a09b76ab8167863c9bfcbe4733f0fc0dc853cf91
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-05-06 12:11:43 +0300

    Add files via upload

---
+++ film_uygulama-1473/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace film_uygulama_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtFılmad = new System.Windows.Forms.TextBox();
+            this.txtYonetmen = new System.Windows.Forms.TextBox();
+            this.txtPuan = new System.Windows.Forms.TextBox();
+            this.txtPoster = new System.Windows.Forms.TextBox();
+            this.txtSure = new System.Windows.Forms.TextBox();
+            this.txtYıl = new System.Windows.Forms.TextBox();
+            this.btnEkleFrom = new System.Windows.Forms.Button();
+            this.btnGuncele = new System.Windows.Forms.Button();
+            this.btnSıl = new System.Windows.Forms.Button();
+            this.cbOdul = new System.Windows.Forms.CheckBox();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.dgwFılmler = new System.Windows.Forms.DataGridView();
+            this.pbResim = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFılmler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FİLM ADI=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "YÖNETMEN=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(29, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "YIL=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(29, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TÜR=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(29, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SURE=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(29, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "POSTER=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(29, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "IMDB puan=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(64, 315);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "ÖDÜL=";
+            // 
+            // txtFılmad
+            // 
+            this.txtFılmad.Location = new System.Drawing.Point(136, 38);
+            this.txtFılmad.Name = "txtFılmad";
+            this.txtFılmad.Size = new System.Drawing.Size(121, 20);
+            this.txtFılmad.TabIndex = 8;
+            // 
+            // txtYonetmen
+            // 
+            this.txtYonetmen.Location = new System.Drawing.Point(136, 76);
+            this.txtYonetmen.Name = "txtYonetmen";
+            this.txtYonetmen.Size = new System.Drawing.Size(121, 20);
+            this.txtYonetmen.TabIndex = 9;
+            // 
+            // txtPuan
+            // 
+            this.txtPuan.Location = new System.Drawing.Point(136, 269);
+            this.txtPuan.Name = "txtPuan";
+            this.txtPuan.Size = new System.Drawing.Size(121, 20);
+            this.txtPuan.TabIndex = 10;
+            // 
+            // txtPoster
+            // 
+            this.txtPoster.Location = new System.Drawing.Point(136, 227);
+            this.txtPoster.Name = "txtPoster";
+            this.txtPoster.Size = new System.Drawing.Size(121, 20);
+            this.txtPoster.TabIndex = 11;
+            // 
+            // txtSure
+            // 
+            this.txtSure.Location = new System.Drawing.Point(136, 185);
+            this.txtSure.Name = "txtSure";
+            this.txtSure.Size = new System.Drawing.Size(121, 20);
+            this.txtSure.TabIndex = 12;
+            // 
+            // txtYıl
+            // 
+            this.txtYıl.Location = new System.Drawing.Point(136, 111);
+            this.txtYıl.Name = "txtYıl";
+            this.txtYıl.Size = new System.Drawing.Size(121, 20);
+            this.txtYıl.TabIndex = 13;
+            // 
+            // btnEkleFrom
+            // 
+            this.btnEkleFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkleFrom.Location = new System.Drawing.Point(570, 50);
+            this.btnEkleFrom.Name = "btnEkleFrom";
+            this.btnEkleFrom.Size = new System.Drawing.Size(203, 68);
+            this.btnEkleFrom.TabIndex = 14;
+            this.btnEkleFrom.Text = "FİLM EKLE";
+            this.btnEkleFrom.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncele
+            // 
+            this.btnGuncele.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncele.Location = new System.Drawing.Point(570, 127);
+            this.btnGuncele.Name = "btnGuncele";
+            this.btnGuncele.Size = new System.Drawing.Size(203, 70);
+            this.btnGuncele.TabIndex = 15;
+            this.btnGuncele.Text = "FİLM GÜNCELLE";
+            this.btnGuncele.UseVisualStyleBackColor = true;
+            // 
+            // btnSıl
+            // 
+            this.btnSıl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSıl.Location = new System.Drawing.Point(570, 206);
+            this.btnSıl.Name = "btnSıl";
+            this.btnSıl.Size = new System.Drawing.Size(203, 58);
+            this.btnSıl.TabIndex = 16;
+            this.btnSıl.Text = "FİLM SİL";
+            this.btnSıl.UseVisualStyleBackColor = true;
+            // 
+            // cbOdul
+            // 
+            this.cbOdul.AutoSize = true;
+            this.cbOdul.Location = new System.Drawing.Point(136, 315);
+            this.cbOdul.Name = "cbOdul";
+            this.cbOdul.Size = new System.Drawing.Size(15, 14);
+            this.cbOdul.TabIndex = 17;
+            this.cbOdul.UseVisualStyleBackColor = true;
+            this.cbOdul.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Location = new System.Drawing.Point(136, 146);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(121, 21);
+            this.cmbTur.TabIndex = 18;
+            // 
+            // dgwFılmler
+            // 
+            this.dgwFılmler.AllowUserToAddRows = false;
+            this.dgwFılmler.AllowUserToDeleteRows = false;
+            this.dgwFılmler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwFılmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwFılmler.Location = new System.Drawing.Point(17, 373);
+            this.dgwFılmler.Name = "dgwFılmler";
+            this.dgwFılmler.ReadOnly = true;
+            this.dgwFılmler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwFılmler.Size = new System.Drawing.Size(756, 222);
+            this.dgwFılmler.TabIndex = 19;
+            this.dgwFılmler.SelectionChanged += new System.EventHandler(this.dgwFılmler_SelectionChanged);
+            // 
+            // pbResim
+            // 
+            this.pbResim.Location = new System.Drawing.Point(346, 50);
+            this.pbResim.Name = "pbResim";
+            this.pbResim.Size = new System.Drawing.Size(176, 239);
+            this.pbResim.TabIndex = 20;
+            this.pbResim.TabStop = false;
+            this.pbResim.Click += new System.EventHandler(this.pbResim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 680);
+            this.Controls.Add(this.pbResim);
+            this.Controls.Add(this.dgwFılmler);
+            this.Controls.Add(this.cmbTur);
+            this.Controls.Add(this.cbOdul);
+            this.Controls.Add(this.btnSıl);
+            this.Controls.Add(this.btnGuncele);
+            this.Controls.Add(this.btnEkleFrom);
+            this.Controls.Add(this.txtYıl);
+            this.Controls.Add(this.txtSure);
+            this.Controls.Add(this.txtPoster);
+            this.Controls.Add(this.txtPuan);
+            this.Controls.Add(this.txtYonetmen);
+            this.Controls.Add(this.txtFılmad);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFılmler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtFılmad;
+        private System.Windows.Forms.TextBox txtYonetmen;
+        private System.Windows.Forms.TextBox txtPuan;
+        private System.Windows.Forms.TextBox txtPoster;
+        private System.Windows.Forms.TextBox txtSure;
+        private System.Windows.Forms.TextBox txtYıl;
+        private System.Windows.Forms.Button btnEkleFrom;
+        private System.Windows.Forms.Button btnGuncele;
+        private System.Windows.Forms.Button btnSıl;
+        private System.Windows.Forms.CheckBox cbOdul;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.DataGridView dgwFılmler;
+        private System.Windows.Forms.PictureBox pbResim;
+    }
+}
+
---
+++ film_uygulama-1473/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace film_uygulama_1473
+{
+
+    public partial class Form1 : Form
+    {
+        string baglanti = "Server=localhost;Database=film_arsiv;Uid=root;Pwd=;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string klasorYolu = @"poster";
+            if (Directory.Exists(klasorYolu))
+            {
+                Directory.CreateDirectory(klasorYolu);
+
+            }
+            CmdDoldur();
+            DgwDoldur();
+        }
+        void DgwDoldur()
+        {
+            using (MySqlConnection baglan = new MySqlConnection(baglanti))
+            {
+                baglan.Open();
+                string sorgu = "SELECT * FROM filmler;";
+
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+
+
+
+                da.Fill(dt);
+                dgwFılmler.DataSource = dt;
+
+             
+                dgwFılmler.Columns["poster"].Visible= false;
+                dgwFılmler.Columns["yonetmen"].Visible = false;
+            }
+        }
+
+        private void dgwFılmler_SelectionChanged(object sender, EventArgs e)
+        {
+            if(dgwFılmler.SelectedRows.Count > 0)
+            {
+                txtFılmad.Text = dgwFılmler.SelectedRows[0].Cells["Film_ad"].Value.ToString();
+                txtYonetmen.Text = dgwFılmler.SelectedRows[0].Cells["yonetmen"].Value.ToString();
+                txtYıl.Text = dgwFılmler.SelectedRows[0].Cells["yil"].Value.ToString();
+                cmbTur.Text = dgwFılmler.SelectedRows[0].Cells["tur"].Value.ToString();
+                txtSure.Text = dgwFılmler.SelectedRows[0].Cells["sure"].Value.ToString();
+                txtPuan.Text = dgwFılmler.SelectedRows[0].Cells["imdb_puan"].Value.ToString();
+                txtPoster.Text = dgwFılmler.SelectedRows[0].Cells["poster"].Value.ToString();
+
+          
+
+                cbOdul.Checked = Convert.ToBoolean(dgwFılmler.SelectedRows[0].Cells["film_odul"].Value);
+            }
+        }
+
+        private void pbResim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void CmdDoldur()
+        {
+            using (MySqlConnection baglan = new MySqlConnection(baglanti))
+            {
+                baglan.Open();
+                string sorgu = "SELECT DISTINCT tur FROM filmler;";
+
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+
+
+
+                da.Fill(dt);
+                cmbTur.DataSource = dt;
+
+                cmbTur.DisplayMember = "tur";
+                cmbTur.ValueMember = "tur";
+            }
+        }
+    }
+}
